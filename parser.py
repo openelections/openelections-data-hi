194e8c05a8e87b8ac80e5ddd6c7435c78736ce51
Derek Willis <dwillis@gmail.com>
2020-12-20 10:52:16 -0500

    updated parser

--- parser.py
+++ parser.py
@@ -3,21 +3,21 @@
 import csv
 import requests
 
-OFFICES = ['President', 'Governor', 'Representative', 'State Senator', 'SELECT A PARTY', 'Lieutenant Governor', 'U.S. Senator']
+OFFICES = ['President and Vice President', 'Governor', 'U.S. Representative', 'State Senator', 'State Representative', 'Lieutenant Governor', 'U.S. Senator']
 
-precinct_file = open("precincts.txt", "r")
+precinct_file = open("precincts.txt", "rt")
 csvfile = csv.DictReader(precinct_file, delimiter=',')
 precincts = list(csvfile)
 
 def general():
     results = []
-    url = "http://files.hawaii.gov/elections/files/results/2008/general/media.txt"
+    url = "https://elections.hawaii.gov/wp-content/results/media.txt"
     r = requests.get(url)
-    decoded_content = r.content
-    reader = csv.DictReader(decoded_content.splitlines(), delimiter=',', encoding='utf-8')
+    decoded_content = r.text
+    reader = csv.DictReader(decoded_content.splitlines())
     for row in reader:
-        county = next((p['COUNTY'] for p in precincts if row['Precinct Name'] == p['PRECINCT']), None)
-        office = row['Contest Title']
+        county = next((p['COUNTY'] for p in precincts if row['Precinct_Name'] == p['PRECINCT']), None)
+        office = row['Contest_title']
         if 'Dist' in office:
             office, district = office.split(', Dist ')
             if district == 'I':
@@ -28,15 +28,12 @@ def general():
                 district = "2"
         else:
             district = None
-        party, candidate = row['Candidate Name'].split(') ')
-        party = row['Candidate Party']
-        votes = row['Total Votes']
-        results.append([county, row['Precinct Name'], office, district, party, candidate, None, None, None, row['Total Votes']])
-        for col in ['Total Blank Votes', 'Total Over Votes', 'Total Ballots']:
-            results.append([county, row['Precinct Name'], office, district, None, col, None, None, None, row[col]])
+        party = row['Choice_party']
+        votes = int(row['Absentee_votes']) + int(row['Early_votes']) + int(row['Election_Votes'])
+        results.append([county, row['Precinct_Name'], office, district, party, row['Candidate_name'], row['Absentee_votes'], row['Early_votes'], row['Election_Votes'], votes])
 
-    with open('2008/20081104__hi__general__precinct.csv','wb') as csvfile:
-            csvwriter = csv.writer(csvfile, encoding='utf-8')
+    with open('2020/20201103__hi__general__precinct.csv','wt') as csvfile:
+            csvwriter = csv.writer(csvfile)
             csvwriter.writerow(['county','precinct', 'office', 'district', 'party', 'candidate', 'absentee', 'early_votes', 'election_day', 'votes'])
             csvwriter.writerows(results)
 
